e2775e74b1d5849bf4c5db7924e1b0dd066e60dd
JrDong <idongjr@gmail.com>
2017-02-19 22:33:46 +0800

    快速排序算法

--- app/src/main/java/com/djr/commonlibrary/algorithm/sort/QuickSort.java
+++ app/src/main/java/com/djr/commonlibrary/algorithm/sort/QuickSort.java
@@ -1,5 +1,8 @@
 package com.djr.commonlibrary.algorithm.sort;
 
+import android.app.ActivityManager;
+import android.widget.EdgeEffect;
+
 /**
  * Created by DongJr on 2017/2/19.
  */
@@ -7,13 +10,53 @@ package com.djr.commonlibrary.algorithm.sort;
 public class QuickSort {
 
 
-    public static void quickSort() {
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
 
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start >= end)
+            return;
+
+        int mid = arr[end];
+        int left = start;
+        int right = end - 1;
+
+        while (left < right) {
+            while (arr[left] <= mid && left < right) {
+                left++;
+            }
+            while (arr[right] >= mid && left < right) {
+                right--;
+            }
+            swap(arr, left, right);
+        }
+
+        if (arr[left] >= arr[end]) {
+            swap(arr, left, end);
+        } else {
+            left++;
+        }
+        quickSort(arr, start, left - 1);
+        quickSort(arr, left + 1, end);
+
+        print(arr);
+    }
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
     }
 
 
     public static void main(String[] args) {
+        int[] arr = {5, 3, 12, 4, 1, 2, 8, 6, 1, 11};
 
+        quickSort(arr, 0, arr.length - 1);
     }
 
 }
